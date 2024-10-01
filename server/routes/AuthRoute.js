7e93c1d1c6581a43b170ba801ba78a1a61454b03
Priyanshu sharma <priyanshus8282@gmail.com>
2024-10-01 14:26:39 +0530

    changes

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import validator from 'validator';
 import dotenv from 'dotenv';
 
