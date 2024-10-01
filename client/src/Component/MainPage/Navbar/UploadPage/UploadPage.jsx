7012add77dbc78358e32b730de291eeaf1a9b32a
PRIYANSHU SHARMA <140371901+Priyanshu8282@users.noreply.github.com>
2024-10-01 14:36:02 +0530

    Update UploadPage.jsx

--- client/src/Component/MainPage/Navbar/UploadPage/UploadPage.jsx
+++ client/src/Component/MainPage/Navbar/UploadPage/UploadPage.jsx
@@ -18,7 +18,7 @@ const UploadForm = ({ setUploadedData }) => {
     formData.append('csvFile', file);
 
     try {
-      const res = await axios.post('http://localhost:5000/upload', formData, {
+      const res = await axios.post('https://certificate-generator-g107.onrender.com/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
