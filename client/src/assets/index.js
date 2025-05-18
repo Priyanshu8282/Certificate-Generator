a5f7a4b179187e7357e83d246ec3f47888743d99
Priyanshu sharma <priyanshus8282@gmail.com>
2025-05-18 12:46:21 +0530

    commit

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,5 @@
 import template_1 from './template_1.png';
-import bg  from './bg.png';
 
 
-export { template_1, bg };
+
+export { template_1};
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import '../../public/fonts.css'; 
+  import '../../public/fonts.css'; 
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
